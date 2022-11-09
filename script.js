9712f892b3a8619943206f8326250f8d28fe6828
Mike <michael.kerpelman@gmail.com>
2022-11-09 06:23:36 -0500

    Implement displayLibrary() method to display books in table

--- script.js
+++ script.js
@@ -7,14 +7,26 @@ function Book(title, author, pages, read) {
   this.read = read;
 };
 
-Book.prototype.info = function() {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${ this.read ? "read" : "not yet read"}.`;
-};
-
 function addBookToLibrary() {
   const title = prompt("Enter book title:");
   const author = prompt("Enter book author:");
   const pages = prompt("Enter number of pages in book:");
   const read = prompt("Have you read this book? (true/false)");
   library.push(new Book(title, author, pages, read));
+};
+
+library.push(new Book("The Hobbit", "JRR Tolkien", 395, true));
+library.push(new Book("The Lord of the Rings", "JRR Tolkien", 1000, false));
+
+function displayLibrary() {
+  library.forEach((book) => {
+    const tr = document.createElement('tr');
+    for (let key in book) {
+      td = document.createElement('td')
+      td.textContent = book[key]
+      tr.appendChild(td)
+    }
+    let tableBody = document.querySelector('table').lastElementChild;
+    tableBody.appendChild(tr)
+  });
 };
