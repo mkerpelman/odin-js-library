591668d2a0219737378345122798f91a4d1070cc
Mike <michael.kerpelman@gmail.com>
2022-11-09 05:53:09 -0500

    Implement addBookToLibrary() method to add new book to library array

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let library = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,4 +9,12 @@ function Book(title, author, pages, read) {
 
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${ this.read ? "read" : "not yet read"}.`;
+};
+
+function addBookToLibrary() {
+  const title = prompt("Enter book title:");
+  const author = prompt("Enter book author:");
+  const pages = prompt("Enter number of pages in book:");
+  const read = prompt("Have you read this book? (true/false)");
+  library.push(new Book(title, author, pages, read));
 };
